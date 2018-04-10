5fa9b077438a658ef6e1290da5c44957343b69eb
Nick Koeppen <nrkoeppe@nps.edu>
2018-04-10 11:35:43 -0700

    Finished with turtle drawing an n

---
+++ scripts/turtleletter.sh
@@ -0,0 +1,2 @@
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, 1.8]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[4.0, 0.0, 0.0]' '[0.0, 0.0, -3.5]'
